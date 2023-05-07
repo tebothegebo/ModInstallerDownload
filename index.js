0cbbe87bc0d6c6c34a74af7e609a12f6650863b9
tebothegebo <132794810+tebothegebo@users.noreply.github.com>
2023-05-06 21:26:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const installer = document.getElementById('installer');
+installer.addEventListener('click', function downloadInstaller() {
+	window.open('https://drive.google.com/drive/u/0/folders/1UagV_Vn11i2HfzGdr9bUAPxTKhyIkvLf');
+})
